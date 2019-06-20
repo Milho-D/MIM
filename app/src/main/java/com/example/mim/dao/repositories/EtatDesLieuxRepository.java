8c0cedde300ec513a205d4e0f4131ae14c276b9c
maklourson <m.makadji87@gmail.com>
2019-06-20 09:49:24 +0200

    Repository ContratLoc et EtatdesLieux

---
+++ app/src/main/java/com/example/mim/dao/repositories/EtatDesLieuxRepository.java
@@ -0,0 +1,51 @@
+package com.example.mim.dao.repositories;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import com.example.mim.dao.AppDatabase;
+import com.example.mim.dao.Connexion;
+import com.example.mim.dao.EtatDesLieuxDao;
+import com.example.mim.model.EtatDesLieux;
+
+import java.util.List;
+
+
+public class EtatDesLieuxRepository {
+    private EtatDesLieuxDao etatDesLieuxDao;
+    private LiveData<List<EtatDesLieux>> getAllEtatDesLieux;
+
+    EtatDesLieuxRepository(Application application){
+        AppDatabase db = Connexion.getConnexion(application);
+
+        etatDesLieuxDao = db.etatDesLieuxDao();
+        getAllEtatDesLieux = etatDesLieuxDao.getAll();
+    }
+
+
+    public LiveData<List<EtatDesLieux>> getAllEtatDesLieux() {
+        return getAllEtatDesLieux;
+    }
+
+
+    public void insert(EtatDesLieux etatDesLieux){
+        new insertAsync(etatDesLieuxDao).execute(etatDesLieux);
+    }
+
+    private static class insertAsync extends AsyncTask<EtatDesLieux,Void,Void> {
+
+        private EtatDesLieuxDao etatDesLieuxDaosync;
+
+        insertAsync(EtatDesLieuxDao etatDesLieuxDao){
+            etatDesLieuxDaosync = etatDesLieuxDao ;
+        }
+
+        @Override
+        protected Void doInBackground(EtatDesLieux... etatDesLieuxes) {
+            etatDesLieuxDaosync.insertAll(etatDesLieuxes);
+            return null;
+        }
+
+    }
+}
